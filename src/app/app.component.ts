f1c4e59765e1c519c7578d34864ab4afdb012157
Mikyas Wolde <Mikyas.Wolde@aristotle.com>
2017-05-22 14:32:29 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  total = null;
+  newValue = null;
+  newName = null;
+
+  items = [];
+
+  add() {
+    this.items.push({ value: this.newValue, name: this.newName });
+    this.recalculate();
+  }
+
+  remove(index) {
+    this.items.splice(index, 1);
+    this.recalculate();
+  }
+
+  recalculate() {
+    this.newValue = null
+    this.newName = null
+    var total = this.items.reduce(function (total, num) { return total + +num.value }, 0);
+    this.items.forEach(function (el) {
+      el.percent = el.value / total;
+    })
+    this.total = total;
+  }
+
+  changing(item) {
+    item.beingEdited = true;
+  }
+
+  changed(item) {
+    item.beingEdited = false;
+    this.recalculate();
+  }
 }
